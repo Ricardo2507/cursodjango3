6fd8ec068152bcfd53332e492953067358237d07
Ricardo <rscarvalho@hotmail.com>
2021-05-21 16:30:43 -0300

    Aula sobre comandos do sistema de templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def hello_blog(request):
+    lista = ['Ricardo', 'Patrícia', 'Necy',' Carolina', 'Da Paz']
+    data = {'name': 'Curso de Django 3', 'lista_nomes': lista}
+    return render(request, 'index.html', data)
